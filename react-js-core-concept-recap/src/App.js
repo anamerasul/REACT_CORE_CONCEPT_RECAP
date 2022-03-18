97fd5307897f5da80d931df6af0770afcc528f8c
anamerasul <anamerasul@gmail.com>
2022-03-18 18:04:39 +0600

    Load data using the useEffect and display data

--- react-js-core-concept-recap/src/App.js
+++ react-js-core-concept-recap/src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 // import React, { Component } from "react"; 
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 function App() {
   return (
     <div className="App">
+      <LoadPosts></LoadPosts>
     {/* <Welcome></Welcome>   */}
     <District name='noakhail' bivag='chitta'></District>
     <District name='bogra' bivag='raj'></District>
@@ -36,6 +37,51 @@ const doublePower=()=>setPower(power*2)
   )
 }
 
+function LoadPosts(){
+
+  const[posts,SetPosts]=useState([])
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then(res=>res.json())
+    .then(data=>SetPosts(data))
+  }, [])
+  
+return(
+  <div>
+    <h1>Post:{posts.length}</h1>
+
+    <div style={{
+      display:'grid',
+      gridTemplateColumns:'repeat(4,1fr)'
+    }}>
+    {
+      posts.map(post=><Post title={post.title}
+      key={post.id}
+      body={post.body}
+      ></Post>)
+    }
+
+    </div>
+  
+  </div>
+)
+}
+
+
+function Post(props){
+
+  return(
+    <div style={{
+      backgroundColor:'gray',
+      padding:'20px',
+      margin:'10px',
+      borderRadius:'20px'
+    }}>
+      <h2>Title:{props.title}</h2>
+      <p>Body:{props.body}</p>
+    </div>
+  )
+}
 
 
 // class Welcome extends Component {
